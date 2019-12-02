15bc0728330f1899a0fd51c7bf8880fa05659b3a
hhzl <hannes.hirzel@gmail.com>
2019-12-02 10:01:55 +0000

    pictures added to learn-mode-page

--- src/app/pages/learn-mode/learn-mode.page.ts
+++ src/app/pages/learn-mode/learn-mode.page.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './learn-mode.page.html',
   styleUrls: ['./learn-mode.page.scss'],
 })
+
 export class LearnModePage implements OnInit {
+
+
+  word: string = 'fish'; 
+  translate : string = 'der Fisch';
+  
+  itemImageUrlPath: string = 'assets/data/pictures/';
+  itemImageFileName : string = 'fish.jpg'; 
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+  
+
+  updateMyValue() {
+     if (this.word == 'fish') {
+              this.word = 'grasshopper';
+              this.translate = 'die Heuschrecke';
+              this.itemImageFileName = 'grasshopper.jpg' 
+        } else
+        {
+              this.word = 'fish';
+              this.translate = 'der Fisch';
+              this.itemImageFileName = 'fish.jpg' 
+        }
   }
 
 }
